842594f4f51672d494831711f4a531f70813509c
mark_dappollone@comcast.com <Mark Dappollone>
2018-07-27 11:52:11 -0400

    Add testutils and sample usage

---
+++ sample/src/test/java/com/xfinity/resourceprovider/sample/ResourceProviderTestUtils.kt
@@ -0,0 +1,35 @@
+package com.xfinity.resourceprovider.sample
+
+import com.nhaarman.mockito_kotlin.whenever
+import com.xfinitymobile.resourceprovider.testutils.DrawableProviderAnswer
+import com.xfinitymobile.resourceprovider.testutils.IntegerProviderAnswer
+import com.xfinitymobile.resourceprovider.testutils.StringProviderAnswer
+import org.mockito.Mockito
+
+fun ResourceProvider.mock() {
+    this.mockStrings()
+    this.mockDrawables()
+    this.mockDimens()
+    this.mockColors()
+    this.mockIntegers()
+}
+
+fun ResourceProvider.mockStrings() {
+    whenever(this.strings).thenReturn(Mockito.mock(StringProvider::class.java, StringProviderAnswer()))
+}
+
+fun ResourceProvider.mockDrawables() {
+    whenever(this.drawables).thenReturn(Mockito.mock(DrawableProvider::class.java, DrawableProviderAnswer()))
+}
+
+fun ResourceProvider.mockColors() {
+    whenever(this.colors).thenReturn(Mockito.mock(ColorProvider::class.java, IntegerProviderAnswer()))
+}
+
+fun ResourceProvider.mockDimens() {
+    whenever(this.dimens).thenReturn(Mockito.mock(DimensionProvider::class.java, IntegerProviderAnswer()))
+}
+
+fun ResourceProvider.mockIntegers() {
+    whenever(this.integers).thenReturn(Mockito.mock(IntegerProvider::class.java, IntegerProviderAnswer()))
+}
---
+++ testutils/src/main/java/com/xfinitymobile/resourceprovider/testutils/ResourceProviderAnswers.kt
@@ -0,0 +1,62 @@
+package com.xfinitymobile.resourceprovider.testutils
+
+import android.graphics.drawable.Drawable
+import org.mockito.Mockito
+import org.mockito.invocation.InvocationOnMock
+import org.mockito.stubbing.Answer
+import java.lang.reflect.Method
+
+/**
+ * This file contains Mockito Answer sub-classes for automatically mocking ResourceProvider function calls.
+ * Refer to the sample application MainPresenterTest and ResourceProviderTestUtils classes for usage examples
+ */
+
+class DrawableProviderAnswer : Answer<Any?> {
+    private val delegate = Mockito.RETURNS_DEFAULTS!!
+    private val drawableMap = mutableMapOf<Method, Drawable>()
+
+    override fun answer(invocation: InvocationOnMock?): Any? {
+        val invocationMethodReturn = invocation?.method?.returnType
+        val drawableType = Drawable::class.java
+        return when (invocationMethodReturn) {
+            drawableType -> {
+                val mappedMock = drawableMap.get(invocation.method)
+                if (mappedMock != null) {
+                    mappedMock
+                } else {
+                    val mock = Mockito.mock(Drawable::class.java)
+                    drawableMap.put(invocation.method, mock)
+                    mock
+                }
+            }
+            else -> delegate.answer(invocation)
+        }
+    }
+}
+
+class StringProviderAnswer : Answer<Any?> {
+    private val delegate = Mockito.RETURNS_DEFAULTS!!
+
+    override fun answer(invocation: InvocationOnMock?): Any? {
+        val invocationMethodReturn = invocation?.method?.returnType
+        val stringType = String::class.java
+        return when (invocationMethodReturn) {
+            stringType -> invocation.method?.name.toString() + invocation.arguments.joinToString()
+            else -> delegate.answer(invocation)
+        }
+    }
+
+}
+
+class IntegerProviderAnswer : Answer<Any?> {
+    private val delegate = Mockito.RETURNS_DEFAULTS!!
+
+    override fun answer(invocation: InvocationOnMock?): Any? {
+        val invocationMethodReturn = invocation?.method?.returnType
+        val intType = Int::class.java
+        return when (invocationMethodReturn) {
+            intType -> invocation.method?.name?.hashCode()
+            else -> delegate.answer(invocation)
+        }
+    }
+}
