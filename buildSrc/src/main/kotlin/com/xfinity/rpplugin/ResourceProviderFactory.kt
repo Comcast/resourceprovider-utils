ab6a35ca614b4dff8687f767bd3c85141f472416
Mark Dappollone <daptronic@gmail.com>
2020-04-27 14:33:43 -0400

    Convert gradle tasks to a gradle plugin and integrate into sample app

--- buildSrc/src/main/kotlin/com/xfinity/rpplugin/ResourceProviderFactory.kt
+++ buildSrc/src/main/kotlin/com/xfinity/rpplugin/ResourceProviderFactory.kt
@@ -1,16 +1,52 @@
 package com.xfinity.rpplugin
 
+import org.gradle.api.Plugin
+import org.gradle.api.Project
 import java.io.File
 import java.util.StringTokenizer
 
+class ResourceProviderPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        // Add the 'greeting' extension object
+        // Add a task that uses configuration from the extension object
+        val extension = project.extensions.create<ResourceProviderPluginExtension>("resourceprovider", ResourceProviderPluginExtension::class.java)
+        project.task("generateResourceProvider") {
+            it.doLast {
+                val rClassFile = File(project.buildDir.toString() + "/intermediates/compile_and_runtime_not_namespaced_r_class_jar/debug/")
+                project.exec {
+                    it.workingDir = rClassFile
+                    it.executable = "unzip"
+                    it.args = listOf("R.jar")
+                    it.isIgnoreExitValue = true
+                }
+
+                project.exec {
+                    it.workingDir = rClassFile
+                    it.commandLine = listOf("bash", "-c", "find ${rClassFile.absolutePath} -name '*.class' | xargs javap -p > rclass.txt")
+                }
+
+                extension.applicationId?.let {
+                    val resourceProviderFactory = ResourceProviderFactory()
+                    resourceProviderFactory.buildResourceProvider(it, project.buildDir.toString(),
+                            "${project.buildDir.toString()}/generated/source/kapt/debug")
+                }
+            }
+        }
+    }
+}
+
+open class ResourceProviderPluginExtension {
+    var applicationId: String? = null
+}
+
 class ResourceProviderFactory {
-    fun buildResourceProvider(packageName: String, outputDirectory: String) {
-        val rpCodeGenerator  =  RpCodeGenerator(packageName, parseRClassInfoFile(), outputDirectory)
+    fun buildResourceProvider(packageName: String, buildDirectory: String, outputDirectory: String) {
+        val rpCodeGenerator  =  RpCodeGenerator(packageName, parseRClassInfoFile(buildDirectory), outputDirectory)
         rpCodeGenerator.generateCode()
     }
 
-    private fun parseRClassInfoFile(): RClassInfo {
-        val rClassInfo = File("/Users/mdappo200/Projects/resourceprovider/sample/build/intermediates/compile_and_runtime_not_namespaced_r_class_jar/debug/rclass.txt").readText()
+    private fun parseRClassInfoFile(buildDirectory: String): RClassInfo {
+        val rClassInfo = File("$buildDirectory/intermediates/compile_and_runtime_not_namespaced_r_class_jar/debug/rclass.txt").readText()
         val tokenizer = StringTokenizer(rClassInfo, "$")
 
         val rClassStringVars = mutableListOf<String>()
